dd158b43beb0cf0e98fb8a8af1311b5ed691ce9f
ikaro almeida <seupaizinho123@gmail.com>
2023-09-14 13:02:24 -0300

    alteração

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,11 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import Formulario from './Components/Form';
+import React,{useState} from 'react';
+
 
 function App() {
+const [input, setInput] = useState("");
+const [tasks, setTasks] = useState([]);
+
   return (
     <div className="App">
       
       <div className="Container">
+
+        <div className='form'>
+          <Formulario
+          input={input}
+          setInput={setInput}
+          tasks={tasks}
+          setTasks={setTasks}
+          />
+        </div>
 
       </div>
 
---
+++ to-do-list/src/Components/Form.js
@@ -0,0 +1,27 @@
+import React, {useState} from 'react';
+import '../App.css'
+import {v4 as uuidv4} from "uuid";
+
+
+const Form = ({input, setInput, tasks, setTasks}) =>{
+const onInputChange = (event) => {
+    setInput(event.target.value);
+}
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        setTasks([...tasks, {id: uuidv4(), title: input, completed: false}]);
+        setInput("");
+    }
+    return(
+
+        <div className='form'>
+        <form className='form__container'> 
+            <input type='text' placeholder='Insira uma nova tarefa' className='input__form' value={input} required onChange={onInputChange}/>
+            <button className='button__form' type='submit'>Adicionar</button>
+        </form>
+        </div>
+    )
+}
+
+export default Form
